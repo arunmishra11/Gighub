13c1bf30f998ac976a561ef7301b22d5e2b9fd89
George <ghern5234@gmail.com>
2024-05-23 15:09:49 -0500

    fixed server and created necessary pages using handlebars

---
+++ dbFunction.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+function readDb(dbName = 'db.json') {
+    const data = fs.readFileSync(dbName,'utf-8')
+    return JSON.parse(data)
+}
+
+function writeDb(obj, dbName = 'db.json'){
+    if(!obj) { return console.log('please provide data to save')}
+    try {
+      fs.writeFileSync(dbName, JSON.stringify(obj))
+
+        return console.log('sucess')
+    } catch(err) {
+      return console.log('failed')
+    }
+}
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require('./controllers'); // Import routes from controllers direc
 const helpers = require('./utils/helpers'); // Import helper function file from utils folder
 
 const sequelize = require('./config/connection'); // Import the Sequelize instance for database connection configuration
-const SequelzeStore = require('connect-session-sequelize')(session.Store); // Import the Sequelize store for session storage
+const SequelizeStore = require('connect-session-sequelize')(session.Store); // Import the Sequelize store for session storage
 
 
 const app = express(); // Set up express application
@@ -16,16 +16,31 @@ const PORT = process.env.PORT || 3001; // Define the port the server will listen
 const hbs = exphbs.create({helpers}); // Set up handbars engine, passing in the helpers function
 
 // Define a session configuration object named 'sess'
-const session = {};
+const sess = {
+    secret: 'Secret',
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+        httpOnly: true,
+        secure: true,
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    })
+};
 app.use(session(sess)); // Use the session middleware with the configuration set to sess. This sets up session management for the express application.
 
-app.engine('handlebars', hbs.engine); // Define template engine to use
-app.set('view engine', 'handlebars'); // Set template engine
+app.engine('handlebars', hbs.engine); // Creates handlebars template engine to use
+app.set('view engine', 'handlebars'); // Sets handlebars template engine
 
 app.use(express.json()); // Middleware to parse JSON data in the request body
 app.use(express.urlencoded({ extended: true })) // Middleware to parse URL-encoded data in the request body, extended: true allows for rich(?) objeects and arrays to be encoded into the URL-encoded format
 app.use(express.static(path.join(__dirname, 'public'))); //Middleware to serve static files from the 'public' directory
 app.use(routes); // Set the routes object for various endpoints in the application
+
+app.get('/', (req, res) => res.render('index')); // Setting the home page
+app.get('/landing', (req, res) => res.render('landing')); // Setting the home page
 
 // Sync the sequelize models with the database
 // force: false ensures that existing tables are not dropped and recreated, preserving data
