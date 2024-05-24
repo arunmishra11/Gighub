649acb736f39677b8a9884a900d0923414612942
George <ghern5234@gmail.com>
2024-05-24 15:59:50 -0500

    set up controller, deleted routes folder

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public'))); //Middleware to serve s
 app.use(routes); // Set the routes object for various endpoints in the application
 
 app.get('/', (req, res) => res.render('index')); // Setting the home page
-app.get('/landing', (req, res) => res.render('landing')); // Setting the home page
+// app.get('/landing', (req, res) => res.render('landing')); // Setting the home page
 app.get('/gigDisplay', (req, res) => res.render('gigDisplay')); // Setting the home page
 app.get('/gigPost', (req, res) => res.render('gigPost'));
 
