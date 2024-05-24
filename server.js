b61cfc134d2673693302308e6d93af050eed1703
George <ghern5234@gmail.com>
2024-05-23 19:01:25 -0500

    added routes to server.js, initiated handlebars pages, created env, imported logo imgs

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ app.use(routes); // Set the routes object for various endpoints in the applicati
 
 app.get('/', (req, res) => res.render('index')); // Setting the home page
 app.get('/landing', (req, res) => res.render('landing')); // Setting the home page
+app.get('/gigDisplay', (req, res) => res.render('gigDisplay')); // Setting the home page
+app.get('/gigPost', (req, res) => res.render('gigPost'));
 
 // Sync the sequelize models with the database
 // force: false ensures that existing tables are not dropped and recreated, preserving data
