cf3cc0803b667353427ac964b66763ae3bdfa1a2
George <ghern5234@gmail.com>
2024-05-26 03:36:25 -0500

    env update and home routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,5 @@
+
+app.get('/', (req, res) => res.render('index')); // Setting the home page
+// app.get('/landing', (req, res) => res.render('landing')); // Setting the home page
+app.get('/gigDisplay', (req, res) => res.render('gigDisplay')); // Setting the home page
+app.get('/gigPost', (req, res) => res.render('gigPost'));
