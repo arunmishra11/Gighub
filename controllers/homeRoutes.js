d6c082d7093e04ad7af267f5c8c34bfcbe774cb6
George <ghern5234@gmail.com>
2024-05-28 19:12:50 -0500

    attempted fixed gigRepo bug

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,31 +4,27 @@ const { Gig, User } = require('../models');
 const router = express.Router();
 
 router.get('/', (req, res) => res.render('index')); // Setting the home page
-router.get('/gigDisplay', async (req, res) => { 
+
+
+router.get('/gigRepo', async (req, res) => { 
     try {
         // Get all projects and JOIN with user data
-        const gigData = await Gig.findAll({
-          include: [
-            {
-              model: User,
-              attributes: ['name'],
-            },
-          ],
-        });
-    
+        const gigData = await Gig.findAll();
+        console.log('hello')
         // Serialize data so the template can read it
         const gigs = gigData.map((gig) => gig.get({ plain: true }));
         console.log(req.session);
+
+        console.log(gigs)
         // Pass serialized data and session flag into template
         res.render('gigRepo', { 
-          gigs, 
-          logged_in: req.session.logged_in 
+          gigs 
         });
       } catch (err) {
-        res.status(500).json(err);res.render('gigDisplay')}; // Setting the home page
+        res.status(500).json(err)}; // Setting the home page
 })
 router.get('/gigPost', (req, res) => res.render('gigPost'));
-router.get('/gigRepo', (req,res) => res.render('gigRepo'));
+// router.get('/gigRepo', (req,res) => res.render('gigRepo'));
 
 
 module.exports = router;
