4e3e1f6d073251cbf28d27d20153bad1ca46b53b
George <ghern5234@gmail.com>
2024-05-31 17:54:33 -0700

    updated gigPost.js in the public folder. Form has appropriate fields and is able to access input data. Need to run fetch calls

--- public/js/gigPost.js
+++ public/js/gigPost.js
@@ -1,1 +1,27 @@
 // Create a card for carasousel
+const form = document.querySelector('#gigpostForm');
+// console.log('Hello')
+// console.log(form)
+
+
+const submitGig = async (event) => {
+    event.preventDefault();
+    console.log('function')
+    const company = document.querySelector('#company').value.trim();
+    const technologies = document.querySelector('#technologies').value.trim();
+    const budget = document.querySelector('#budget').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const description = document.querySelector('#description').value.trim();
+
+    console.log(company);
+    console.log(technologies);
+    console.log(budget);
+    console.log(email);
+    console.log(description);
+
+    //if statment
+    // need to make fetch 
+
+}
+
+form.addEventListener('submit', submitGig);
