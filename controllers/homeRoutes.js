88859eb64854398692ff3d561abe7964cee69e46
Rualexandra <rualexandra99@gmail.com>
2024-06-01 22:05:28 -0500

    added working carousel previews and contact email links

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,61 +1,72 @@
-
-const express = require('express');
-const { Gig, User } = require('../models');
+const express = require("express");
+const { Gig, User } = require("../models");
 const router = express.Router();
 
-router.get('/', (req, res) => res.render('index')); // Setting the home page
+// router.get('/', (req, res) => res.render('index')); // Setting the home page
+
+router.get("/", async (req, res) => {
+  try {
+    const gigData = await Gig.findAll();
+    const gigs = gigData.map((gig) => gig.get({ plain: true }));
+
+    res.render("index", {
+      gigs,
+      publicPath: "/public", // path to public****
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}); // Setting the home page for carousel
 
 // Render full list of gigs on gigRepo
-router.get('/gigRepo', async (req, res) => { 
-    try {
-        // Get all projects and JOIN with user data
-        const gigData = await Gig.findAll();
-        // Serialize data so the template can read it
-        const gigs = gigData.map((gig) => gig.get({ plain: true }));
-        console.log(req.session);
+router.get("/gigRepo", async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const gigData = await Gig.findAll();
+    // Serialize data so the template can read it
+    const gigs = gigData.map((gig) => gig.get({ plain: true }));
+    console.log(req.session);
 
-        console.log(gigs)
-        // Pass serialized data and session flag into template
-        res.render('gigRepo', { 
-          gigs 
-        });
-      } catch (err) {
-        res.status(500).json(err)}; // Setting the home page
-})
+    console.log(gigs);
+    // Pass serialized data and session flag into template
+    res.render("gigRepo", {
+      gigs,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  } // Setting the home page
+});
 
 // Search db for gig and render to gigPost
-router.get('/gig/:id', async (req, res) => {
-    try {
-      const gigsData = await Gig.findByPk(req.params.id, {
-        include: [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-        ],
-      });
-  
-      const gig = gigsData.get({ plain: true });
-  
-      res.render('gigDisplay', {
-        ...gig,
-        // logged_in: req.session.logged_in
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+router.get("/gig/:id", async (req, res) => {
+  try {
+    const gigsData = await Gig.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
 
+    const gig = gigsData.get({ plain: true });
+
+    res.render("gigDisplay", {
+      ...gig,
+      // logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Render gigPost page for creating a new gig
-router.get('/gigPost', (req, res) => res.render('gigPost'));
-
+router.get("/gigPost", (req, res) => res.render("gigPost"));
 
 // Render individual gig
-router.get('/gigDisplay', (req, res) => res.render('gigDisplay'));
+router.get("/gigDisplay", (req, res) => res.render("gigDisplay"));
 
 // Render login/signup page
-router.get('/loginSignup', (req, res) => res.render('loginSignup'));
-
+router.get("/loginSignup", (req, res) => res.render("loginSignup"));
 
 module.exports = router;
