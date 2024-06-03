018d6a90e0a0a23de8e57f09d65549d92fbe35c0
arunmishra305@gmail.com <arunmishra305@gmail.com>
2024-06-03 15:10:03 -0500

    adding login to direct gigpost page once successful login

--- public/js/userLogin.js
+++ public/js/userLogin.js
@@ -1,23 +1,24 @@
-const loginbtn = document.getElementById("loginbtn")
-async function loginForm(e){
-    e.preventDefault()
-const email = document.getElementById("email").value
-const password = document.getElementById("password").value
+const loginbtn = document.getElementById("loginbtn");
+async function loginForm(e) {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-const response = await fetch("/api/users/login",{
-method:"POST",
-body:JSON.stringify({
-    email,password
-}),
-headers:{'Content-Type':'application/json'}
-})
-console.log(response)
-if (response.ok){
-     window.location.replace('/gigpost')
+  const response = await fetch("/api/users/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+  console.log(response);
+  window.location.replace("/gigpost");
+  if (response.ok) {
+    window.location.replace("/gigpost");
+  } else {
+    alert("Something went wrong");
+    console.log(response.statusText);
+  }
 }
-else {
-    alert("Something went wrong")
-}
-loginbtn.addEventListener("click",loginForm )
-}
-
+loginbtn.addEventListener("click", loginForm);
