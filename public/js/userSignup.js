f688d1c4ffffc4749d25e59fea5a76f9bb10df7e
arunmishra11 <156019606+arunmishra11@users.noreply.github.com>
2024-06-04 18:06:24 -0500

    make login working again

--- public/js/userSignup.js
+++ public/js/userSignup.js
@@ -1,37 +1,28 @@
-const signupForm = document.querySelector("#signupForm");
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("loginForm");
 
+  async function loginFormHandler(e) {
+    e.preventDefault();
 
-// Function to add new user information
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-const signupFunction = async function (event) {
-  event.preventDefault();
-  const firstName = document.querySelector("#firstName").value.trim();
-  const lastName = document.querySelector("#lastName").value.trim();
-  const email = document.querySelector("#email").value.trim();
-  const password = document.querySelector("#password").value.trim();
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-  console.log(firstName, lastName, email, password);
-
-  if (firstName && lastName && email && password) {
-    const name = firstName + " " + lastName;
-    // fetch
-    const res = await fetch("/api/users/",{
-        method: 'POST',
-        body: JSON.stringify({
-          name,email,password
-        }),
-        headers: {"Content-Type":"application/json"}
-    } )
-    if (res.ok) {
-      window.location.replace("/");
+    if (response.ok) {
+      window.location.replace("/gigPost");
     } else {
       alert("Something went wrong");
-      console.log(res.statusText);
+      console.log(response.statusText);
     }
-  
   }
-};
 
-
-
-signupForm.addEventListener("submit", signupFunction);
+  loginForm.addEventListener("submit", loginFormHandler);
+});
