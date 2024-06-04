914937d46fc2a54840ff9b32baaeadb75a8d5977
George <ghern5234@gmail.com>
2024-06-03 20:53:31 -0700

    started userSignup.js

---
+++ public/js/userSignup.js
@@ -0,0 +1,24 @@
+const signupForm = document.querySelector("#signupForm");
+
+const signupFunction = async function (event) {
+  event.preventDefault();
+  const firstName = document.querySelector("#firstName").value.trim();
+  const lastName = document.querySelector("#lastName").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
+
+  console.log(firstName, lastName, email, password);
+
+  if (firstName && lastName && email && password) {
+    const name = firstName + " " + lastName;
+    console.log(name);
+    //fetch
+    const res = await fetch("/api/users/ ",{
+        method: 'POST',
+        body:
+        Headers:
+    } )
+  }
+};
+
+signupForm.addEventListener("submit", signupFunction);
