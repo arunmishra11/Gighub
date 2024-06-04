b26c112fdddc09d31d60cc73d1aa225f7748e05e
George <ghern5234@gmail.com>
2024-06-04 13:19:38 -0700

    cleaned up unnecessary code

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,22 +1,16 @@
-// Create a card for carasousel
+
 console.log('Connected...')
-//const form = document.querySelector('#gigpostForm');
+
 const logoutBtn = document.querySelector('#logout');
-// console.log('Hello')
-// console.log(form)
+
 
 
 const logoutFn = async (event) => {
-    /*
-    event.preventDefault();
-    console.log('function')
-    const firstName = document.querySelector('#first-name').value.trim();
-    const lastName = document.querySelector('#last-name').value.trim();
-    */
+
     console.log("Submitting logout")
     await fetch('/api/users/logout', { method: 'POST' })
     window.location.replace('/');
 }
 
-// form.addEventListener('submit', submitGig);
+
 logoutBtn.addEventListener('click', logoutFn);
